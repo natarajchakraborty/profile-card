6d4d128e14070a2fb31187b1093521af95564b2b
Nataraj Chakraborty <nataraj@MacBook-Pro.local>
2021-06-10 23:19:52 +0530

    tailwind setup initial html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,30 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend : {
+      fontFamily: {
+        'body': ['Kumbh Sans']
+      },
+      fontSize: {
+        'base18': '1.125rem'
+      },
+      screens: {
+        'xsm': '375px',
+        '1xl': '1440px'
+      },
+      colors: {
+        darkcyan: '#19a2ae',
+        darkDesaturatedBlue: '#2d3248',
+        darkGrayishBlue: '#6a6f81',
+        darkGray: '#969696'
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['hover', 'focus'],
+    },
+    display: ['responsive', 'group-hover', 'group-focus'],
   },
   plugins: [],
 }
